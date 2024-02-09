4d4ab649d3a944cdd782a6a3257183c6e3c63691
Arif Hossain <alivearif120@gmail.com>
2024-02-09 11:40:07 +0100

    change

--- user_profile/lib/Animations/FadeAnimation.dart
+++ user_profile/lib/Animations/FadeAnimation.dart
@@ -1,31 +1,44 @@
-// import 'package:flutter/widgets.dart';
-// //import 'package:simple_animations/simple_animations.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-// class FadeAnimation extends StatelessWidget {
-//   final double delay;
-//   final Widget child;
+// import 'package:simple_animations/simple_animations.dart';
+//import 'package:flutter/animation.dart';
 
-//   const FadeAnimation({super.key, required this.delay, required this.child});
-//   @override
-//   Widget build(BuildContext context) {
-//     final tween = MultiTrackTween(
-//       [
-//         Track("Opacity").add(
-//           Duration(milliseconds: 500),
-//           Tween(
-//             begin: 0.0,
-//             Track("translateY").add(
-//                 Duration(microseconds: 500), Tween(begin: 130.0, end: 0.0),
-//                 curve: Curves.easeOut),
-//           ),
-//         ),
-//       ],
-//     );
-//     return ControledAnimation(
-//       delay: Duration(milliseconds:( 400*delay).round(),
-//    ),
-//    duration:tween.duration, tween:tween, child:child);
+//enum AniProps {opacity,translateY, track}
 
-    
-//   }
-// }
+class FadeAnimation extends StatelessWidget {
+  final double delay;
+  final Widget child;
+
+  const FadeAnimation({super.key, required this.delay, required this.child});
+  @override
+  Widget build(BuildContext context) {
+    final tween = MultiTween([
+        Track("Opacity").add(
+          const Duration(milliseconds: 500),
+          Tween(
+            begin: 0.0, end:1.0,),),
+            Track("translateY").add(
+              const  Duration(microseconds: 500),
+                Tween(begin: 130.0, end: 0.0),
+                curve: Curves.easeOut),
+         ] );
+    return PlayAnimation(
+      delay: Duration(
+        milliseconds: (400 * delay).round(),
+      ),
+      duration: tween.duration,
+      tween: tween,
+      child: child,
+      builderWithChild: (context, child, animation) => Opacity(
+        opacity: animation["opacity"],
+        child: Transform.translate(
+          offset: Offset(
+            0,
+            animation["translateY"],
+          ),
+        ),
+      ),
+    );
+  }
+ }
